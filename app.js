87eeffc4bab4260463792b7f73edfe9402668329
Jacob Skovmand Christensen <jacobskovmand@hotmail.com>
2026-06-09 12:28:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
 
-const express = require("express");
-const { createClient } = require("@supabase/supabase-js");
-
 const app = express();
 app.use(express.json());
 
