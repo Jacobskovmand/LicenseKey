b6ad0580ccbb786657122a48c372d14134308e6d
Jacob Skovmand Christensen <jacobskovmand@hotmail.com>
2026-06-10 08:07:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log("SUPABASE_KEY:", process.env.SUPABASE_KEY ? "OK" : "MISSING");
 
 const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
